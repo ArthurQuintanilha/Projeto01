56726464eb6aac7b4788fe87ebffcde3af0d03ce
Arthur Quintanilha <aquintanilhaduarte@gmail.com>
2023-04-13 21:36:50 -0300

    Validações nos formulários

--- src/app/pages/add-tarefa/add-tarefa.page.ts
+++ src/app/pages/add-tarefa/add-tarefa.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Tarefa } from 'src/app/model/tarefa';
 import { TarefaService } from 'src/app/services/tarefa.service';
@@ -8,14 +8,14 @@ import { ActivatedRoute } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { NavController } from '@ionic/angular'
 import { Validators } from '@angular/forms';
- 
+
 
 @Component({
   selector: 'app-add-tarefa',
   templateUrl: './add-tarefa.page.html',
   styleUrls: ['./add-tarefa.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class AddTarefaPage implements OnInit {
   tarefa: Tarefa;
@@ -25,14 +25,7 @@ export class AddTarefaPage implements OnInit {
 
 
 
-  constructor(private fBuider: FormBuilder, private activatedRoute: ActivatedRoute, private toastController: ToastController, private navController: NavController) {
-    this.fGroup = this.fBuider.group({
-      'descricricao':  [''],
-      'data': [''],
-      'situacao': ['']
-
-
-    });
+  constructor(private fBuilder: FormBuilder, private activatedRoute: ActivatedRoute, private toastController: ToastController, private navController: NavController) {
 
     this.tarefaService = new TarefaService();
     let id = this.activatedRoute.snapshot.params['id'];
@@ -42,19 +35,53 @@ export class AddTarefaPage implements OnInit {
     } else {
       this.tarefa = new Tarefa();
     }
+
+    this.fGroup = this.fBuilder.group({
+
+      descricao: this.tarefa.descricao,
+
+      termino: this.tarefa.termino,
+
+      situacao: this.tarefa.situacao,
+    });
+
   }
 
   ngOnInit() {
+    this.fGroup = this.fBuilder.group({
+      descricao:
+        ['', Validators.compose([
+          Validators.required,
+        ])],
 
+      termino:
+        ['', Validators.compose([
+          Validators.required
+        ])],
 
-   }
+      situacao: ['']
+    });
+  }
+
+  createForm() {
+    this.fGroup = this.fBuilder.group({
+      descricao: new FormControl(this.tarefa.descricao),
+      termino: new FormControl(this.tarefa.termino),
+      situacao: new FormControl(this.tarefa.situacao),
+
+    })
+  }
 
   salvar() {
-    this.tarefaService.salvar(this.tarefa);
-
-    this.exibirMensagem('Registro salvo com sucesso!!!');
-    this.navController.navigateBack('/tarefa');
+    console.log(this.fGroup.value);
+    if (this.fGroup.valid) {
+      this.createForm();
+      this.tarefaService.salvar(this.tarefa);
+      this.exibirMensagem('Registro salvo com sucesso!!!');
+      this.navController.navigateBack('/tarefa');
+    }
   }
+
   async exibirMensagem(texto: string) {
 
     const toast = await this.toastController.create({
@@ -70,3 +97,5 @@ export class AddTarefaPage implements OnInit {
 
 
 }
+
+
